650fe8c729078fb8866642fe597331b2f566173b
TawiahObedCodeX <tawiaho858@gmail.com>
2025-12-16 14:07:42 +0000

    done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,26 +1,22 @@
-import {createBrowserRouter,createRoutesFromElements,Route, RouterProvider, Outlet} from "react-router-dom"
-import Home from "./Pages/Home"
-import Login from "./Pages/Login"
-import Explore from "./Pages/Explore"
-import GigsPage from "./Pages/GigsPage"
-import Signup from "./Pages/Signup"
-
-
-
-
-
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Explore from "./Pages/Explore";
+import GigsPage from "./Pages/GigsPage";
+import Signup from "./Pages/Signup";
 
 export default function App() {
-  const router= createBrowserRouter(createRoutesFromElements(
-      <Route  >
-        <Route path="/" element={<Home/>} />
-        <Route path='/login' element={< Login/>} />
-        <Route  path='/signup' element={<Signup/>} />
-        <Route path='/Explore' element={<Explore />} />
-        <Route path="/Gigs" element={<GigsPage/>} />
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/explore" element={<Explore />} /> {/* lowercase */}
+        <Route path="/gigs" element={<GigsPage />} />  {/* lowercase */}
       </Route>
-  ))
-  return (
-    <RouterProvider router={router} />
-  )
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
